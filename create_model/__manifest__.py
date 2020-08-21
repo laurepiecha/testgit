bb55b5fec3ed19a553e20a0c38dd9845f9c795a4
diederdas2020 <63906577+diederdas2020@users.noreply.github.com>
2020-08-21 15:54:35 +0200

    Update __manifest__.py

--- create_model/__manifest__.py
+++ create_model/__manifest__.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-{
-    'name': "CreateModel",
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-    'description': """
-        Long description of module's purpose
-    """,
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+{y.com",
     'category': 'Uncategorized',
 jjjjjjjjjjjjjjjjjjjjjjjjjjjj
         diederdas
